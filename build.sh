c157099816cece1064e0887da76580cb7b158635
j-hry <j-hry@users.noreply.github.com>
2025-07-26 18:02:25 +0800

    Changed build script back after superuser creation

--- build.sh
+++ build.sh
@@ -8,9 +8,5 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Force re-run the superuser migration
-python manage.py migrate app 0001 --fake
-python manage.py migrate app 0002
-
 # Run all other migrations
 python manage.py migrate
