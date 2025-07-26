9d8ac42544476dfbe9a3acb5cf0a9d40761b0918
j-hry <j-hry@users.noreply.github.com>
2025-07-26 18:09:51 +0800

    Mark migration 0003 as fake to resolve creator_id column conflict

--- build.sh
+++ build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+# Mark migration 0003 as fake to avoid creator_id conflict
+python manage.py migrate app 0003 --fake
+
 # Run all other migrations
 python manage.py migrate
