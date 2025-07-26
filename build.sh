fe17ff0bc5c106d98bd1aa6696dfe49662abbe1c
j-hry <j-hry@users.noreply.github.com>
2025-07-26 17:58:05 +0800

    Add fake-initial to handle fresh database

--- build.sh
+++ build.sh
@@ -8,5 +8,9 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
-# Run migrations
+# Force re-run the superuser migration
+python manage.py migrate app 0001 --fake
+python manage.py migrate app 0002
+
+# Run all other migrations
 python manage.py migrate
